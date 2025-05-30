bc8909fe3ad1d292cd7fa76b09dcc4f6a86ccf34
khaled2672 <mohamedkh2672@gmail.com>
2025-05-31 00:05:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import LinearRegression
-from xgboost import XGBRegressor
-from lightgbm import LGBMRegressor
-from catboost import CatBoostRegressor
-import os
+
 
 st.set_page_config(page_title="Power Prediction App", layout="wide")
 st.title("🔌 Power Prediction using Ensemble Models")
